73561867b5bdf76a66ab57d652e63be23f5957c8
RubIZH <get1595@gmail.com>
2023-07-29 22:26:41 -0600

    Added: Crossed text hero section

--- components/organism/Header.vue
+++ components/organism/Header.vue
@@ -5,30 +5,60 @@
         class="relative max-w-screen-xl mx-auto pt-20 sm:pt-24 lg:pt-60 flex"
       >
         <div class="flex w-full justify-center">
-          <div class="flex flex-col space-y-12">
-            <h1
-              class="
-                flex
-                font-serif
-                text-[8.75rem]
-                tracking-tight
-                leading-[7.5rem]
-              "
-            >
-              Rubén
-            </h1>
+          <div class="flex flex-col space-y-12 text-amethyst-900">
+            <div class="flex">
+              <div class="grow-0 relative">
+                <div
+                  class="
+                    w-[calc(100%+2rem)]
+                    -m-[1rem]
+                    h-2.5
+                    absolute
+                    bg-[#FAC515]
+                    top-[60%]
+                  "
+                ></div>
+                <h1
+                  class="
+                    tracking-tight
+                    leading-[7.5rem]
+                    relative
+                    mix-blend-hard-light
+                    font-serif
+                    text-[8.75rem]
+                  "
+                >
+                  Ruben
+                </h1>
+              </div>
+            </div>
+
             <div class="flex space-x-24">
               <div class="h-28 w-28 bg-amethyst-400"></div>
-              <h1
-                class="
-                  font-serif
-                  text-[8.75rem]
-                  tracking-tight
-                  leading-[7.5rem]
-                "
-              >
-                Hidalgo
-              </h1>
+              <div class="grow-0 relative">
+                <div
+                  class="
+                    w-[calc(100%+2rem)]
+                    -m-[1rem]
+                    h-2.5
+                    absolute
+                    bg-[#FAC515]
+                    top-[60%]
+                  "
+                ></div>
+                <h1
+                  class="
+                    tracking-tight
+                    leading-[7.5rem]
+                    relative
+                    mix-blend-hard-light
+                    font-serif
+                    text-[8.75rem]
+                  "
+                >
+                  Hidalgo
+                </h1>
+              </div>
             </div>
           </div>
         </div>
