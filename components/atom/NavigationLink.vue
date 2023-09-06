16553f23779bfc7d74ce8d51452885b0e7749b80
RubIZH <get1595@gmail.com>
2023-09-05 21:03:52 -0600

    1 file modified, 2 added

---
+++ components/atom/NavigationLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <NuxtLink
+    class="
+      text-md
+      font-semibold
+      py-1
+      px-4
+      text-gray-600
+      border
+      rounded-full
+      border-gray-400
+      dark:border-white dark:text-white
+      bg-gradient-to-b
+      from-gray-300
+      to-white
+      dark:bg-none
+      hover:from-gray-400 hover:to-gray-100
+      dark:hover:bg-gray-800
+    "
+  >
+    <slot></slot>
+  </NuxtLink>
+</template>
---
+++ components/atom/SelectedNavigationLink.vue
@@ -0,0 +1,53 @@
+<template>
+  <NuxtLink
+    class="
+      group
+      border
+      rounded-full
+      border-amethyst-300
+      dark:border-amethyst-700
+      bg-gradient-to-b
+      from-amethyst-500 from-60%
+      to-amethyst-100
+      dark:from-amethyst-800
+      dark:from-60%
+      dark:to-amethyst-500
+      dark:hover:from-amethyst-700
+      dark:hover:from-60%
+      dark:hover:to-amethyst-400
+      hover:from-amethyst-600 hover:from-60% hover:to-amethyst-300
+      relative
+      px-4
+      py-1
+    "
+  >
+    <span
+      class="
+        z-[1]
+        relative
+        text-md
+        font-semibold
+        text-amethyst-800
+        dark:text-white
+        group-hover:text-amethyst-900 group-hover:dark:text-amethyst-100
+      "
+    >
+      <slot></slot>
+    </span>
+    <div
+      class="
+        z-[0]
+        w-[calc(100%-8px)]
+        h-[calc(100%-8px)]
+        rounded-full
+        absolute
+        inset-x-[4px]
+        top-0
+        bg-gradient-to-b
+        from-white from-10%
+        opacity-95
+        dark:opacity-40
+      "
+    ></div>
+  </NuxtLink>
+</template>
